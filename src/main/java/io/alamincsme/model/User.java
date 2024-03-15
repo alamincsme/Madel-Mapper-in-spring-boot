d7044fad9be4b0cb337b02d5cc79979bb7cb5457
alamincsme <alamincsme@gmail.com>
2024-03-15 19:52:22 +0600

    User

---
+++ src/main/java/io/alamincsme/model/User.java
@@ -0,0 +1,14 @@
+package io.alamincsme.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private long id ;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
